03f559362d3cc2cd97b05db329ca78714ee37a38
Spatay2000 <Spatay.148@gmail.com>
2023-06-26 17:27:05 +0600

    delete +2

--- lib/src/size_extension.dart
+++ lib/src/size_extension.dart
@@ -6,10 +6,10 @@ import 'screen_util.dart';
 
 extension SizeExtension on num {
   ///[ScreenUtil.setWidth]
-  double get w => ScreenUtil().setWidth(this + 2);
+  double get w => ScreenUtil().setWidth(this);
 
   ///[ScreenUtil.setHeight]
-  double get h => ScreenUtil().setHeight(this + 2);
+  double get h => ScreenUtil().setHeight(this);
 
   ///[ScreenUtil.radius]
   double get r => ScreenUtil().radius(this);
@@ -21,7 +21,7 @@ extension SizeExtension on num {
   double get dm => ScreenUtil().diameter(this);
 
   ///[ScreenUtil.setSp]
-  double get sp => ScreenUtil().setSp(this + 2);
+  double get sp => ScreenUtil().setSp(this);
 
   ///smart size :  it check your value - if it is bigger than your value it will set your value
   ///for example, you have set 16.sm() , if for your screen 16.sp() is bigger than 16 , then it will set 16 not 16.sp()
